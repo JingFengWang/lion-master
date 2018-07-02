82a4caa60199cabf2a367c01e7b19734d25ef169
wangjingfeng <wangjingfeng@kingsoft.com>
2018-07-02 15:25:33 +0800

    201807021525

---
+++ lion-spark/lion-spark-core/src/main/scala/spark/deploy/Command.scala
@@ -0,0 +1,10 @@
+package spark.deploy
+
+import scala.collection.Map
+
+private[spark]
+case class Command(
+    mainClass: String,
+    arguments: Seq[String],
+    environment: Map[String, String]) {
+}
